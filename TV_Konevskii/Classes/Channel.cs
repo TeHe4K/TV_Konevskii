0a7c89ea451f47a3a2fb3f4b9792d41a54218c0c
Lobanov Mikhail <axxuetam@gmail.com>
2025-09-29 11:34:17 +0500

    Разработанный класс Channel. Коневский

---
+++ TV_Konevskii/Classes/Channel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TV_Konevskii.Classes
+{
+    internal class Channel
+    {
+        public string Name { get; set; }
+        public string Src { get; set; }
+    }
+}
